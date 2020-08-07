7efe9c373b607d8d246f974d55889dd2547ddf42
ttolson-code <ttolson@protonmail.com>
2020-08-07 17:10:19 -0400

    Moved router to index.js and moved app.js component.

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import AppNav from './components/app/AppNav';
+import LandingPage from './components/landing-page/LandingPage'
+import FishFinderIndex from './components/fish-finder/FishFinderIndex';
+import NewsIndex from './components/news/NewsIndex';
+import './App.css';
+
+const App = () => {
+ 
+  return (
+    <div className="app-container">
+      <AppNav />
+
+      <Switch>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+
+        <Route path="/fish-finder">
+          <FishFinderIndex />
+        </Route>
+
+        <Route path="/news">
+          <NewsIndex />
+        </Route>
+
+        <Redirect to="/" />
+      </Switch>
+    </div>
+  )
+}
+
+export default App;
