07969c7fb77f690155aa8cdbd7d0ab20d51683a5
ttolson-code <ttolson@protonmail.com>
2020-08-07 17:08:16 -0400

    Added global navbar.

---
+++ frontend/src/components/app/AppNav.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './css/appNav.css'
+
+const AppNav = () => {
+  return(
+    <nav className="appNav-container">
+      <NavLink exact to="/" className="appNav-link">Home</NavLink>
+      <NavLink 
+        to="/fish-finder/all-profiles" 
+        className="appNav-link" 
+        isActive={() => { 
+          if(window.location.pathname.includes("fish-finder")) {
+            return true;
+          }
+        }}
+      >
+        Fish Finder
+      </NavLink>
+      <NavLink to="/news" className="appNav-link">News</NavLink>
+    </nav>
+  )
+}
+
+export default AppNav;
