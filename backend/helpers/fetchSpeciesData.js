606f633e355835b7f6ba741abb21e97df6103af6
ttolson-code <ttolson@protonmail.com>
2020-08-07 17:20:12 -0400

    Updated function name. Removed comments.

--- backend/helpers/fetchSpeciesData.js
+++ backend/helpers/fetchSpeciesData.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import { MongoClient } from 'mongodb';
-import { mongoGet } from './mongoConnection';
+import { getMongoConnection } from './mongoConnection';
 
 // Use node-fetch to fetch data from FishWatch.gov api.
 async function fetchSpeciesData() {
@@ -18,7 +18,7 @@ async function fetchSpeciesData() {
 
 export default async function runCron() {
   const [ speciesData ] = await Promise.all([ fetchSpeciesData() ]);
-  const db = mongoGet();
+  const db = getMongoConnection();
   
   // Drop species collection in order to reseed with fresh data.
   console.log('Dropping species collection.')
@@ -31,29 +31,3 @@ export default async function runCron() {
   });
   console.log('Cron Job complete.')
 }
-
-
-
-// Create mongoDB connection.
-// const mongoURL = 'mongodb://localhost:27017';
-// const dbName = 'fish-finder';
-// const mongoClient = new MongoClient(mongoURL, {useNewUrlParser: true, useUnifiedTopology: true });
-
-// IIFE async function to establish connection.
-// (async function() {
-//   try {
-//     // await mongoClient.connect();
-//     // console.log("Connected successfully to MongoDB server.");
-
-//     // const db = mongoClient.db(dbName);
-//     // const collection = db.collection('species');
-//     // Take speciesData array and insert individual species objects into mongoDB as documents.
-//     speciesData.map(species => {
-//       console.log("Inserting Document.");
-//       collection.insertOne(species);
-//     });
-//   } catch (err) {
-//     console.log(err.stack);
-//   }
-//   console.log("Cron Job Complete.");
-  // })();
