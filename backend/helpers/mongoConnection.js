602b72d5cf345c9183185157a34981ebd945fc8b
ttolson-code <ttolson@protonmail.com>
2020-08-07 16:55:19 -0400

    Renamed function.

--- backend/helpers/mongoConnection.js
+++ backend/helpers/mongoConnection.js
@@ -4,22 +4,21 @@ import config from '../config';
 // Assign variables from config.js.
 const { mongoDb: { mongoUrl, dbName, options } } = config;
 
-let connection = null;
+let connection;
 
 const mongoConnect = () => new Promise((resolve, reject) => {
-  MongoClient
-    .connect(mongoUrl, options, (err, db) => {
-      if (err) { reject(err); return; };
-      resolve(db);
-      connection = db;
+  MongoClient.connect(mongoUrl, options, (err, db) => {
+    if (err) { reject(err); return; };
+    resolve(db);
+    connection = db;
   });
 });
 
-const mongoGet = () => {
+const getMongoConnection = () => {
   if(!connection) {
     throw new Error('Call connect first!');
   }
   return connection.db(dbName);
 }
 
-export { mongoConnect, mongoGet };
+export { mongoConnect, getMongoConnection };
