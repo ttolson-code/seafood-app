4ca22f0333e1ccd55fd39af3418a754b59dc0d86
ttolson-code <ttolson@protonmail.com>
2020-08-07 17:18:28 -0400

    Recoded landing page.

---
+++ frontend/src/components/landing-page/LandingPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './css/landingPage.css'
+
+const LandingPage = (props) => {
+
+  return (
+    <div className="landing-container">
+
+      <div className="landing-header-container">
+        <header className="landing-header">Seafood App</header>
+      </div>
+
+      <div className="landing-button-container">
+        <Link className="landing-button" to="/fish-finder/all-profiles">Fish Finder</Link>
+        <Link className="landing-button" to="/news">News</Link>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
