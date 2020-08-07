a761cb8464f84cbd892a1f2ccc09439a4d608f5b
ttolson-code <ttolson@protonmail.com>
2020-08-07 16:57:10 -0400

    Updated cron job time.

--- backend/helpers/cronJobs.js
+++ backend/helpers/cronJobs.js
@@ -1,8 +1,8 @@
 import cron from 'node-cron';
 import runCron from './fetchSpeciesData';
 
-// Run cron job every 10 minutes. 
-cron.schedule(`*/10 * * * *`, () => {
+// Run cron job every 12 hours. 
+cron.schedule(`0 */12 * * *`, () => {
   console.log(`Running Cron Job.`);
   runCron();
 });
