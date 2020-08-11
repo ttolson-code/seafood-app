e3a8c524d351af15a662664eb3a34fb847fd67aa
ttolson-code <ttolson@protonmail.com>
2020-08-11 01:07:55 -0400

    Updated species profile UI and cleaned up code.

--- frontend/src/components/fish-finder/SpeciesProfile.js
+++ frontend/src/components/fish-finder/SpeciesProfile.js
@@ -4,117 +4,80 @@ import './css/speciesProfile.css'
 
 const SpeciesProfile = ({ speciesList }) => {
   const params = useParams();
-  const profileId = params.profileId;
 
-  const speciesInfo = speciesList.filter(species => species['Species Name'] === profileId);
-  console.log(speciesInfo[0]['Species Name']);
+  const speciesId = params.speciesId;
+  const speciesFind = speciesList.filter(species => species['Path'] === `/profiles/${speciesId}`);
+  const speciesInfo = speciesFind[0];
 
   return (
     <div className="species-profile-container">
 
-      <div className="species-profile-header">
-        <h1><em>{speciesInfo[0]['Scientific Name']}</em></h1>
+      <img className="fishFinder-species-profile-illustration" src={speciesInfo["Species Illustration Photo"].src} alt={speciesInfo["Species Illustration Photo"].alt}></img>
+
+      <div className="species-profile-scientific">
+        <h1><em>{speciesInfo['Scientific Name']}</em></h1>
       </div>
 
-      <img className="fishFinder-species-profile-illustration" src={speciesInfo[0]["Species Illustration Photo"].src} alt={speciesInfo[0]["Species Illustration Photo"].alt}></img>
-
-      <br />
-
-      <div>
+      <div className="species-profile-alias">
         <h3>Also known as:</h3>
         <p>
-          {speciesInfo[0]["Species Aliases"].replace(/(<([^>]+)>)/ig, '')}
+          {speciesInfo["Species Aliases"].replace(/(<([^>]+)>)/ig, '')}
         </p>
       </div>
 
-      <br />
-
       <div className="species-profile-quote">
         <h3>
-          {speciesInfo[0]['Quote']}
+          {speciesInfo['Quote']}
         </h3>
       </div>
 
       <div className="species-profile-info-container">
 
-        <div>
-          <h3>Population:</h3>
-          <p>
-            {speciesInfo[0]["Population"]}
-          </p>
-        </div>
+        <h2>Harvest Information</h2>
 
-        <br />
+        <table>
+          <tr>
+            <th>Population:</th>
+            <td>{speciesInfo["Population"]}</td>
+          </tr>
+          <tr>
+            <th>Source:</th>
+            <td>{speciesInfo["Source"].replace(/(<([^>]+)>)/ig, '')}</td>
+          </tr>
+          <tr>
+            <th>Habitat Impacts:</th>
+            <td>{speciesInfo["Habitat Impacts"]}</td>
+          </tr>
+          <tr>
+            <th>Fishing Rate:</th>
+            <td>{speciesInfo["Fishing Rate"]}</td>
+          </tr>
+          <tr>
+            <th>By Catch:</th>
+            <td>{speciesInfo["Bycatch"]}</td>
+          </tr>
+        </table>
 
-        <div>
-          <h3>Source:</h3>
-          <p>
-            {speciesInfo[0]["Source"].replace(/(<([^>]+)>)/ig, '')}
-          </p>
-        </div>
+        <h2>Market Information</h2>
 
-        <br />
-
-        <div>
-          <h3>Habitat Impacts:</h3>
-          <p>
-            {speciesInfo[0]["Habitat Impacts"]}
-          </p>
-        </div>
-
-        <br />
-
-        <div>
-          <h3>Fishing Rate:</h3>
-          <p>
-            {speciesInfo[0]["Fishing Rate"]}
-          </p>
-        </div>
-
-        <br />
-
-        <div>
-          <h3>By Catch:</h3>
-          <p>
-            {speciesInfo[0]["Bycatch"]}
-          </p>
-        </div>
-
-        <br />
-
-        <div>
-          <h3>Availability:</h3>
-          <p>
-            {speciesInfo[0]["Availability"].replace(/(<([^>]+)>)/ig, '')}
-          </p>
-        </div>
-
-        <br />
-
-        <div>
-          <h3>Color:</h3>
-          <p>
-          {!speciesInfo[0]["Color"] ? "N/A" : speciesInfo[0]["Color"].replace(/(<([^>]+)>)/ig, '')}
-          </p>
-        </div>
-    
-        <br />
-
-        <div>
-          <h3>Taste:</h3>
-          <p>
-            {speciesInfo[0]["Taste"].replace(/(<([^>]+)>)/ig, '')}
-          </p>
-        </div>
-    
-        <br />
-
-        <div>
-          <h3>Texture:</h3>
-          <p>
-            {!speciesInfo[0]["Texture"] ? "N/A" : speciesInfo[0]["Texture"].replace(/(<([^>]+)>)/ig, '')}
-          </p>
-        </div>
+        <table>
+          <tr>
+            <th>Availability:</th>
+            <td>{speciesInfo["Availability"].replace(/(<([^>]+)>)/ig, '')}</td>
+          </tr>
+          <tr>
+            <th>Color:</th>
+            <td>{!speciesInfo["Color"] ? "N/A" : speciesInfo["Color"].replace(/(<([^>]+)>)/ig, '')}</td>
+          </tr>
+          <tr>
+            <th>Taste:</th>
+            <td>{!speciesInfo["Taste"] ? "N/A" : speciesInfo["Taste"].replace(/(<([^>]+)>)/ig, '')}</td>
+          </tr>
+          <tr>
+            <th>Texture:</th>
+            <td>{!speciesInfo["Texture"] ? "N/A" : speciesInfo["Texture"].replace(/(<([^>]+)>)/ig, '')}</td>
+          </tr>
+        </table>
       </div>
     </div>
   );
