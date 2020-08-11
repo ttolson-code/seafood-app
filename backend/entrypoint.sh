b50ed56e2f263e59efe0f58cdecf331d3248f7dd
Tyler <tg_webb@yahoo.com>
2020-08-10 22:17:13 -0400

    Removed backend entrypoint as it is no longer needed

--- backend/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# perform an npm install before starting server
-npm install node-fetch -g
-
-# node helpers/fetchSpeciesData.js
-
-# exec is used to run the docker CMD after the entrypoint completes.
-exec "$@"
