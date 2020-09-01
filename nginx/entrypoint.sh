1b4330e28b55f07e0bc87062f0facb40bad5cbf5
Tyler <tg_webb@yahoo.com>
2020-09-01 16:22:06 -0400

    Removed unused vars from nginx config

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${FRONT_END}','${BACK_END}','${FRONT_END_PORT}','${BACK_END_PORT}','${FRONT_END_DOMAIN}','${BACK_END_DOMAIN}','${NGINX_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${FRONT_END_DOMAIN}','${BACK_END_DOMAIN}','${NGINX_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
