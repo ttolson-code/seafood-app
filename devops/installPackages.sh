45034c67e179ac241a084598e52f134c208dbb72
Tyler <tg_webb@yahoo.com>
2020-09-04 12:58:39 -0400

    Changed docker to docker-ce

--- devops/installPackages.sh
+++ devops/installPackages.sh
@@ -3,7 +3,7 @@
 function installDependencies() {
     echo "Installing Dependencies..."
     yum update -y
-    yum install jq zip unzip curl python-pip docker -y
+    yum install jq zip unzip curl python-pip docker-ce -y
     curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     ./aws/install
