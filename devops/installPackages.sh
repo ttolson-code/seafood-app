ef56bbb0746a6b5837af51be5b22b9f980dacb29
Tyler <tg_webb@yahoo.com>
2020-09-04 14:05:00 -0400

    Forgot to add installPackages.sh for docker-compose

--- devops/installPackages.sh
+++ devops/installPackages.sh
@@ -11,6 +11,9 @@ function installDependencies() {
     chmod +x /usr/bin/ecs-cli
     echo "Installing amazon linux extras..."
     amazon-linux-extras install docker -y
+    curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 }
 
 ### Main ###
