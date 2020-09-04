1cc648e2a654fdb0f3b95bc510db99ee540c7add
Tyler <tg_webb@yahoo.com>
2020-09-04 14:03:06 -0400

    Use docker-compose to build

--- devops/dockerBuild.sh
+++ devops/dockerBuild.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
 function configureAwsCli() {
+    ECR_LOGIN="$(aws ecr get-login --no-include-email)"
+
     echo "Configuring AWS..."
     aws configure set aws_access_key_id ${ACCESS_KEY}
     aws configure set aws_secret_access_key ${SECRET_KEY}
     aws configure set default.region $AWS_DEFAULT_REGION
     aws configure set default.output json
-    ECR_LOGIN="$(aws ecr get-login --no-include-email)"
     ${ECR_LOGIN}
 }
 
 function buildImages() {
-    REPOSITORY_NAME=$(cat docker-compose-aws.yml | grep 'container_name: ' | cut -d':' -f 2)
-    for REPOS in ${REPOSITORY_NAME}
-    do
-        IMAGE_DIR=$(cat docker-compose-aws.yml | grep 'context: ' | cut -d':' -f 2)
-        docker build -t nginx:latest ./nginx/
-    done
-    docker image ls
+    docker-compose -f docker-compose-aws.yml build
+    # REPOSITORY_NAME=$(cat docker-compose-aws.yml | grep 'container_name: ' | cut -d':' -f 2)
+
+    # for REPOS in $REPOSITORY_NAME
+    # do
+    #   IMAGE_DIR=$(cat docker-compose-aws.yml | grep 'context: ' | cut -d':' -f 2)
+      
+    # done
 }
 
 ### Main ###
@@ -37,9 +39,9 @@ function buildImages() {
 # fi
 
 # export environment variables 
-echo 'export TAG=$(echo ${CIRCLE_SHA1} | head -c 8)' >> $BASH_ENV
-echo 'export DATE=$(date '+%Y-%m-%d')' >> $BASH_ENV
-source $BASH_ENV
+# echo 'export TAG=$(echo ${CIRCLE_SHA1} | head -c 8)' >> $BASH_ENV
+# echo 'export DATE=$(date '+%Y-%m-%d')' >> $BASH_ENV
+# source $BASH_ENV
 
 # configureAwsCli
-# buildImages
+buildImages
