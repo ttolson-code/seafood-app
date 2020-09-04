77320b7d2365903638836ccb130d6f928faf46a7
Tyler <tg_webb@yahoo.com>
2020-09-04 12:48:32 -0400

    Added docker and docker-compose to image

--- devops/installPackages.sh
+++ devops/installPackages.sh
@@ -3,7 +3,7 @@
 function installDependencies() {
     echo "Installing Dependencies..."
     yum update -y
-    yum install jq zip unzip curl python-pip -y
+    yum install jq zip unzip curl python-pip docker docker-compose -y
     curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     ./aws/install
