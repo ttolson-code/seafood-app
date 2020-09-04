d82e6235a25c8e8fcfd930e51a0101dee13b8135
Tyler <tg_webb@yahoo.com>
2020-09-04 12:01:52 -0400

    Testing install package script

---
+++ devops/installPackages.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+function installDependencies() {
+    echo "Installing Dependencies..."
+    yum update -y
+    yum install ${DEPENDENCIES} -y
+    curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    ./aws/install
+    curl -o /usr/bin/ecs-cli "https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest"
+    chmod +x /usr/bin/ecs-cli
+}
+
+### Main ###
+
+DEPENDENCIES=["jq" "zip" "unzip" "curl" "python-pip"]
+
+installDependencies
