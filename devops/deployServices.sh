e1de3eeb4b51ce7a4d0fa4cdbd6332bc156c5849
Tyler <tg_webb@yahoo.com>
2020-09-08 12:13:30 -0400

    Fix ecs-cli argument

--- devops/deployServices.sh
+++ devops/deployServices.sh
@@ -3,7 +3,7 @@ set -ex
 
 function createService() {
 
-  ecs-cli compose --project-name ${SERVICE} --file docker-compose-aws.yaml \
+  ecs-cli compose --project-name ${SERVICE} --file docker-compose-aws.yml \
     --ecs-params deploy-scripts/ecsParams.yml --task-role-arn ${TASK_ROLE_NAME} service up \
     --launch-type FARGATE --create-log-groups \
     --cluster ${CLUSTER_NAME} \
