ed39ee49c393278352b9fad95dec3727a59ef924
Tyler <tg_webb@yahoo.com>
2020-09-08 13:00:58 -0400

    Changed dir for ecsParams.yml

--- devops/deployServices.sh
+++ devops/deployServices.sh
@@ -4,7 +4,7 @@ set -ex
 function createService() {
 
   ecs-cli compose --project-name ${SERVICE} --file docker-compose-aws.yml \
-    --ecs-params deploy-scripts/ecsParams.yml --task-role-arn ${TASK_ROLE_NAME} service up \
+    --ecs-params ./devops/ecsParams.yml --task-role-arn ${TASK_ROLE_NAME} service up \
     --launch-type FARGATE --create-log-groups \
     --cluster ${CLUSTER_NAME} \
     --timeout 15 --target-group-arn ${TARGET_GROUP_ARN} \
