ea02ccb0d0d25618b00c08311686c63855b86240
Tyler <tg_webb@yahoo.com>
2020-09-08 08:53:54 -0400

    Added circleci env vars to dockerBuild

--- devops/dockerBuild.sh
+++ devops/dockerBuild.sh
@@ -12,49 +12,43 @@ function configureAwsCli() {
 }
 
 function buildImages() {
-    # IMAGES=["express-server:latest" "seafood-app:latest" "nginx-proxy:latest" "mongo-gb:latest"]
-
     docker-compose -f docker-compose-aws.yml build
 
-    docker tag seafood-app:latest "${ECR_URI}/seafood-app:latest"
-    docker tag express-server:latest "${ECR_UR}/express-server:latest"
-    docker tag nginx-proxy:latest "${ECR_URI}/nginx-proxy:latest"
-    docker tag mongo-db:latest "${ECR_URI}/mongo-db:latest"
-    # for IMAGE in ${IMAGES}; do
-    #     docker tag "${IMAGE}" "${ECR_URI}/${IMAGE}"
-    #     docker push "${ECR_URI}/${IMAGE}"
-    # done
+    # docker tag seafood-app:latest "${ECR_URI}/seafood-app:latest"
+    # docker tag express-server:latest "${ECR_UR}/express-server:latest"
+    # docker tag nginx-proxy:latest "${ECR_URI}/nginx-proxy:latest"
+    # docker tag mongo-db:latest "${ECR_URI}/mongo-db:latest"
+
+    IMAGES=["mongo-db:latest"]
+
+    for IMAGE in ${IMAGES}; do
+        docker tag "${IMAGE}" "${ECR_URI}/${IMAGE}"
+        docker push "${ECR_URI}/${IMAGE}"
+    done
 
     docker image ls
-    # REPOSITORY_NAME=$(cat docker-compose-aws.yml | grep 'container_name: ' | cut -d':' -f 2)
-
-    # for REPOS in $REPOSITORY_NAME
-    # do
-    #   IMAGE_DIR=$(cat docker-compose-aws.yml | grep 'context: ' | cut -d':' -f 2)
-      
-    # done
 }
 
 ### Main ###
 
 #Ensure CI/CD variables are set otherwise exit
-# if [[ -z "${AWS_REGION}" ]]; then
-#   echo "AWS_REGION is empty"
-#   exit 1
-# fi
-# if [[ -z "${ACCESS_KEY}" ]]; then
-#   echo "ACCESS_KEY is empty"
-#   exit 1
-# fi
-# if [[ -z "${SECRET_KEY}" ]]; then
-#   echo "SECRET_KEY is empty"
-#   exit 1
-# fi
+if [[ -z "${AWS_DEFAULT_REGION}" ]]; then
+  echo "AWS_REGION is empty"
+  exit 1
+fi
+if [[ -z "${ACCESS_KEY}" ]]; then
+  echo "ACCESS_KEY is empty"
+  exit 1
+fi
+if [[ -z "${SECRET_KEY}" ]]; then
+  echo "SECRET_KEY is empty"
+  exit 1
+fi
 
 # export environment variables 
 # echo 'export TAG=$(echo ${CIRCLE_SHA1} | head -c 8)' >> $BASH_ENV
 # echo 'export DATE=$(date '+%Y-%m-%d')' >> $BASH_ENV
 # source $BASH_ENV
 
-# configureAwsCli
+configureAwsCli
 buildImages
