feb6a8138a18fa55ac12697ec5752cae90373be5
ttolson-code <ttolson@protonmail.com>
2022-06-07 21:31:07 -0400

    Renamed endpoint. Sorted news by date.

--- backend/controller/News.controller.js
+++ backend/controller/News.controller.js
@@ -4,12 +4,12 @@ import { getMongoConnection } from '../helpers/mongoConnection';
 
 const NewsController = express.Router();
 
-NewsController.get('/all-news', async (req, res) => {
-  console.log('/all-news endpoint reached.');
+NewsController.get('/all', async (req, res) => {
+  console.log('/all endpoint reached.');
 
   const db = getMongoConnection();
 
-  db.collection('news').find().toArray((err, items) => {
+  db.collection('news').find().sort({"date": 1}).toArray((err, items) => {
     // res.end(JSON.stringify(items));
     res.end(JSON.stringify(items, null, 4));
   })
