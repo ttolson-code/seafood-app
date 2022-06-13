cdfe42fd2898537b5a5f04302f2d52b8bb6b4c24
Tim Olson <ttolson@protonmail.com>
2022-06-13 17:14:41 -0400

    Updated link urls.

--- frontend/src/components/fish-finder/FishFinderNav.js
+++ frontend/src/components/fish-finder/FishFinderNav.js
@@ -9,9 +9,9 @@ const FishFinderNav = () => {
       <label className="fishFinder-search-box-label" htmlFor="Search-box">Find Species:</label>
       <input className="fishFinder-search-box" type="text"/>
     
-      <NavLink to="/fish-finder/profiles/all-profiles" className="fishFinder-search-filter-button">All</NavLink>
-      <NavLink to="/fish-finder/profiles/wild-profiles" className="fishFinder-search-filter-button">Wild</NavLink>
-      <NavLink to="/fish-finder/profiles/farmed-profiles" className="fishFinder-search-filter-button">Farmed</NavLink>
+      <NavLink to="/fish-finder/profiles/all" className="fishFinder-search-filter-button">All</NavLink>
+      <NavLink to="/fish-finder/profiles/wild" className="fishFinder-search-filter-button">Wild</NavLink>
+      <NavLink to="/fish-finder/profiles/farmed" className="fishFinder-search-filter-button">Farmed</NavLink>
     </div>  
   );
 }
