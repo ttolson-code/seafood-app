ff0ef54427e328ae60291c9ef8f8c8d4a4ee2219
Tim Olson <ttolson@protonmail.com>
2022-06-13 17:10:22 -0400

    Added port to config file.

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,12 @@
 const config = {
   express: {
     baseURL: 'http://',
-    host: (process.env.HOST_ADDRESS),
-    port: 5000,
+    host: process.env.HOST_ADDRESS,
+    port: process.env.PORT,
   },
   mongoDb: {
-    mongoUrl: (process.env.DB_URL),
-    dbName: (process.env.DB_NAME),
+    mongoUrl: process.env.DB_URL,
+    dbName: process.env.DB_NAME,
     options: {
       useUnifiedTopology: true,
       useNewUrlParser: true,
