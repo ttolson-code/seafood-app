ee68c1a8dba8f6409c8ec46e5bddf3b030e8fb92
Tim Olson <ttolson@protonmail.com>
2022-06-13 17:13:26 -0400

    Moved router to app.js, react-router 6 updates.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,20 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-// import AppNav from './components/app/AppNav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/landing-page/LandingPage'
 import FishFinderIndex from './components/fish-finder/FishFinderIndex';
 import NewsIndex from './components/news/NewsIndex';
 import './app.css';
 
 const App = () => {
- 
   return (
     <div className="app-container">
-
-      <Switch>
-
-        {/* Url matches exactly / */}
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-        
-        {/* Url matches fish-finder/* */}
-        <Route path="/fish-finder">
-          <FishFinderIndex />
-        </Route>
-        
-        {/* Url matches /news/* */}
-        <Route path="/news">
-          <NewsIndex />
-        </Route>
-        
-        {/* If url does not match /fishfinder/*, /news/*, or "/" redirect to "/" */}
-        <Redirect to="/" />
-      </Switch>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="fish-finder/profiles/*" element={<FishFinderIndex />} />
+          <Route path="news" element={<NewsIndex />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
