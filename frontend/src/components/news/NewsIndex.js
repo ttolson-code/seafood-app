f7e1ffc6149c8ad33efa149ac6c0e35ecea15819
Tim Olson <ttolson@protonmail.com>
2022-06-13 22:29:01 -0400

    Fixed api url.

--- frontend/src/components/news/NewsIndex.js
+++ frontend/src/components/news/NewsIndex.js
@@ -1,26 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { useRouteMatch } from 'react-router-dom';
 import NewsBanner from '../news/NewsBanner';
 import AppHeader from '../app/AppHeader';
 import NewsList from './NewsList';
 
 const NewsIndex = () => {
-  let route = useRouteMatch();
-
   const [newsData, setNewsData] = useState({});
   const [loading, setLoading] = useState(false);
 
   async function fetchNewsAPI() {
     // The fetch url needs to be made an environment variable using process.env in the brackets didnt work
     // const res = await fetch('https://api.seafood-app.com/fish-finder/all-profiles');
-    const res = await fetch('http://localhost:5000/news/all');
+    const res = await fetch('http://localhost:3001/news/all');
     const resJson = await res.json();
     setNewsData(resJson);
     setLoading(true);
   }
-
-  console.log(newsData);
-
   useEffect(() => {
     fetchNewsAPI();
   }, []);
@@ -35,7 +29,7 @@ const NewsIndex = () => {
 
   return (
     <div className="news-container">
-      <AppHeader headerText={route.url}/>
+      <AppHeader />
       <NewsBanner />
       <NewsList newsData={newsData} />
     </div>
