f9edabe684093c2b64e105c2d7fa00855d3fd1e4
Tim Olson <ttolson@protonmail.com>
2022-06-13 22:31:37 -0400

    Added css classes for media queries. Added handleFilterChange prop.

--- frontend/src/components/fish-finder/FishFinderNav.js
+++ frontend/src/components/fish-finder/FishFinderNav.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './css/fishFinderNav.css'
 
-const FishFinderNav = () => {
-
+const FishFinderNav = ({ handleFilterChange }) => {
   return (
-    <div className="fishFinder-search-container">
-      <label className="fishFinder-search-box-label" htmlFor="Search-box">Find Species:</label>
-      <input className="fishFinder-search-box" type="text"/>
-    
-      <NavLink to="/fish-finder/profiles/all" className="fishFinder-search-filter-button">All</NavLink>
-      <NavLink to="/fish-finder/profiles/wild" className="fishFinder-search-filter-button">Wild</NavLink>
-      <NavLink to="/fish-finder/profiles/farmed" className="fishFinder-search-filter-button">Farmed</NavLink>
+    <div className="fishFinder-nav-container">
+      <div className="fishFinder-search-container">
+        <label className="fishFinder-search-box-label" htmlFor="Search-box">Find Species:</label>
+        <input className="fishFinder-search-box" type="text"/>
+      </div>
+      <div className="fishFinder-filter-container">
+        <NavLink to="/fish-finder/profiles/all" className="fishFinder-search-filter-button" onClick={() => handleFilterChange('all')}>All</NavLink>
+        <NavLink to="/fish-finder/profiles/wild" className="fishFinder-search-filter-button" onClick={() => handleFilterChange('wild')}>Wild</NavLink>
+        <NavLink to="/fish-finder/profiles/farmed" className="fishFinder-search-filter-button" onClick={() => handleFilterChange('farmed')}>Farmed</NavLink>
+      </div>
     </div>  
   );
 }
