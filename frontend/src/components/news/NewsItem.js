b44f7a3c40bb5eb3810a68e75c01770fbd666c86
Tim Olson <ttolson@protonmail.com>
2022-06-14 14:40:32 -0400

    Added moment.js library and used to format dates.

--- frontend/src/components/news/NewsItem.js
+++ frontend/src/components/news/NewsItem.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import moment from 'moment'
 import './css/newsItem.css'
 
 const NewsItem = (props) => {
+  const formattedDate = moment(props.date).format('LL');
+  
   return (
     <li className="news-item">
       <div className="news-item-content">
         <h2 className="news-item-heading"><a className="news-item-link"href={props.url} target="_blank">{props.title}</a></h2>
         <p className="news-item-caption">{props.caption}</p>
-        <p className="news-item-date">{props.date}</p>
+        <p className="news-item-date">{formattedDate}</p>
       </div>
       <img className="news-item-image" src={props.imageURL}></img>
     </li>
