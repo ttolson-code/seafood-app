64f77a438f2559ec71fb7fa10f7c48585558cb51
Tim Olson <ttolson@protonmail.com>
2022-06-14 14:57:22 -0400

    Updated SpeciesProfile component to fetch data from api rather than use entire list of species.

--- frontend/src/components/fish-finder/SpeciesProfile.js
+++ frontend/src/components/fish-finder/SpeciesProfile.js
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import species from '../../apis/species';
 import './css/speciesProfile.css'
 
-const SpeciesProfile = ({ speciesList }) => {
-  const params = useParams();
+const SpeciesProfile = () => {
+  const [speciesData, setSpeciesData] = useState('');
+  const [loading, setLoading] = useState(false);
 
+  const params = useParams();
   const speciesId = params.speciesId;
-  const speciesFind = speciesList.filter(species => species['Path'] === `/profiles/${speciesId}`);
-  const speciesInfo = speciesFind[0];
+
+  async function fetchFishFinderAPI(speciesId) {
+    const response = await species.get(`/fish-finder/species/${speciesId}`);
+    setSpeciesData(response.data)
+    setLoading(true);
+  }
+
+  useEffect(() => {
+    if (speciesId) {
+      fetchFishFinderAPI(speciesId);
+    }
+  }, [speciesId]);
+
+  if (!loading) {
+    return (
+      <div className="fishFinder-loader-container">
+        <div className="fishFinder-loader"></div>
+      </div>
+    )
+  }
 
   return (
     <div className="species-profile-container">
-
-      <img className="fishFinder-species-profile-illustration" src={speciesInfo["Species Illustration Photo"].src} alt={speciesInfo["Species Illustration Photo"].alt}></img>
+      <img className="fishFinder-species-profile-illustration" src={speciesData["Species Illustration Photo"].src} alt={speciesData["Species Illustration Photo"].alt}></img>
 
       <div className="species-profile-scientific">
-        <h1><em>{speciesInfo['Scientific Name']}</em></h1>
+        <h1><em>{speciesData['Scientific Name']}</em></h1>
       </div>
 
       <div className="species-profile-alias">
         <h3>Also known as:</h3>
         <p>
-          {speciesInfo["Species Aliases"].replace(/(<([^>]+)>)/ig, '')}
+          {speciesData["Species Aliases"].replace(/(<([^>]+)>)/ig, '')}
         </p>
       </div>
 
       <div className="species-profile-quote">
         <h3>
-          {speciesInfo['Quote']}
+          {speciesData["Quote"]}
         </h3>
       </div>
 
@@ -36,47 +56,51 @@ const SpeciesProfile = ({ speciesList }) => {
         <h2>Harvest Information</h2>
 
         <table>
-          <tr>
-            <th>Population:</th>
-            <td>{!speciesInfo["Population"] ? "N/A" : speciesInfo["Population"]}</td>
-          </tr>
-          <tr>
-            <th>Source:</th>
-            <td>{!speciesInfo["Source"] ? "N/A" : speciesInfo["Source"].replace(/(<([^>]+)>)/ig, '')}</td>
-          </tr>
-          <tr>
-            <th>Habitat Impacts:</th>
-            <td>{!speciesInfo["Habitat Impacts"] ? "N/A" : speciesInfo["Habitat Impacts"]}</td>
-          </tr>
-          <tr>
-            <th>Fishing Rate:</th>
-            <td>{!speciesInfo["Fishing Rate"] ? "N/A" : speciesInfo["Fishing Rate"]}</td>
-          </tr>
-          <tr>
-            <th>By Catch:</th>
-            <td>{!speciesInfo["ByCatch"] ? "N/A" : speciesInfo["Bycatch"]}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <th>Population:</th>
+              <td>{!speciesData["Population"] ? "N/A" : speciesData["Population"]}</td>
+            </tr>
+            <tr>
+              <th>Source:</th>
+              <td>{!speciesData["Source"] ? "N/A" : speciesData["Source"].replace(/(<([^>]+)>)/ig, '')}</td>
+            </tr>
+            <tr>
+              <th>Habitat Impacts:</th>
+              <td>{!speciesData["Habitat Impacts"] ? "N/A" : speciesData["Habitat Impacts"]}</td>
+            </tr>
+            <tr>
+              <th>Fishing Rate:</th>
+              <td>{!speciesData["Fishing Rate"] ? "N/A" : speciesData["Fishing Rate"]}</td>
+            </tr>
+            <tr>
+              <th>By Catch:</th>
+              <td>{!speciesData["ByCatch"] ? "N/A" : speciesData["Bycatch"]}</td>
+            </tr>
+          </tbody>
         </table>
 
         <h2>Market Information</h2>
 
         <table>
-          <tr>
-            <th>Availability:</th>
-            <td>{!speciesInfo["Availability"] ? "N/A" : speciesInfo["Availability"].replace(/(<([^>]+)>)/ig, '')}</td>
-          </tr>
-          <tr>
-            <th>Color:</th>
-            <td>{!speciesInfo["Color"] ? "N/A" : speciesInfo["Color"].replace(/(<([^>]+)>)/ig, '')}</td>
-          </tr>
-          <tr>
-            <th>Taste:</th>
-            <td>{!speciesInfo["Taste"] ? "N/A" : speciesInfo["Taste"].replace(/(<([^>]+)>)/ig, '')}</td>
-          </tr>
-          <tr>
-            <th>Texture:</th>
-            <td>{!speciesInfo["Texture"] ? "N/A" : speciesInfo["Texture"].replace(/(<([^>]+)>)/ig, '')}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <th>Availability:</th>
+              <td>{!speciesData["Availability"] ? "N/A" : speciesData["Availability"].replace(/(<([^>]+)>)/ig, '')}</td>
+            </tr>
+            <tr>
+              <th>Color:</th>
+              <td>{!speciesData["Color"] ? "N/A" : speciesData["Color"].replace(/(<([^>]+)>)/ig, '')}</td>
+            </tr>
+            <tr>
+              <th>Taste:</th>
+              <td>{!speciesData["Taste"] ? "N/A" : speciesData["Taste"].replace(/(<([^>]+)>)/ig, '')}</td>
+            </tr>
+            <tr>
+              <th>Texture:</th>
+              <td>{!speciesData["Texture"] ? "N/A" : speciesData["Texture"].replace(/(<([^>]+)>)/ig, '')}</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
