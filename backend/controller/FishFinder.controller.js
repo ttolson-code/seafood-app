bdf5c6035da4696378a79a6dbf7d843a2fa12ec0
Tim Olson <ttolson@protonmail.com>
2022-06-14 14:58:03 -0400

    Updated apis to use async/await. Fixed individual species endpoint.

--- backend/controller/FishFinder.controller.js
+++ backend/controller/FishFinder.controller.js
@@ -7,10 +7,9 @@ const FishFinderController = express.Router();
 FishFinderController.get('/profiles/all', async (req, res) => {
   console.log('/profiles/all endpoint reached.');
 
-  const db = getMongoConnection();
+  const db = await getMongoConnection();
 
-  db.collection('species').find().sort({"Species Name": 1}).toArray((err, items) => {
-    // res.end(JSON.stringify(items));
+  await db.collection('species').find().sort({"Species Name": 1}).toArray((err, items) => {
     res.end(JSON.stringify(items, null, 4));
   })
 }); 
@@ -19,10 +18,9 @@ FishFinderController.get('/profiles/all', async (req, res) => {
 FishFinderController.get('/profiles/wild', async (req, res) => {
   console.log('/profiles/wild endpoint reached.');
 
-  const db = getMongoConnection();
+  const db = await getMongoConnection();
 
   db.collection('species').find({"Harvest Type":"Wild"}).sort({"Species Name": 1}).toArray((err, items) => {
-    // res.end(JSON.stringify(items));
     res.end(JSON.stringify(items, null, 4));
   })
 });
@@ -31,25 +29,22 @@ FishFinderController.get('/profiles/wild', async (req, res) => {
 FishFinderController.get('/profiles/farmed', async (req, res) => {
   console.log('/profiles/farmed endpoint reached.');
   
-  const db = getMongoConnection();
+  const db = await getMongoConnection();
 
-  db.collection('species').find({"Harvest Type":"Farmed"}).sort({"Species Name": 1}).toArray((err, items) => {
-    // res.end(JSON.stringify(items));
+  await db.collection('species').find({"Harvest Type":"Farmed"}).sort({"Species Name": 1}).toArray((err, items) => {
     res.end(JSON.stringify(items, null, 4));
   })
 });
 
 
 FishFinderController.get('/species/:id', async (req, res) => {
-  
-  const db = getMongoConnection();
+  console.log(`/species/${req.params.id} endpoint reached.`)
 
-  console.log(req.params.id);
-  
-  db.collection('species').find({ "Species Name" : req.params.id }).toArray((err, items) => {
-    // res.end(JSON.stringify(items));
-    res.end(JSON.stringify(items, null, 4));
-  })
+  const db = await getMongoConnection();
+
+  const speciesData = await db.collection('species').findOne({ "Path" : new RegExp(req.params.id) })
+    
+  res.end(JSON.stringify(speciesData, null, 4));
 });
 
 
