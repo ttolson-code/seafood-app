e35aa15741833d496ad6fc47c05a691de44f6ae5
Tim Olson <ttolson@protonmail.com>
2022-06-19 21:22:02 -0400

    Added page spinner component.

---
+++ frontend/src/components/app/PageSpinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageSpinner = () => {
+  return (
+    <div className="fishFinder-loader-container">
+      <div className="fishFinder-loader"></div>
+    </div>
+  )
+}
+
+export default PageSpinner
