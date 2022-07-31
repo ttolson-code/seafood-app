151dfe28efe07341db52840a695fa169e4b63f3a
Tim Olson <ttolson@protonmail.com>
2022-07-31 18:32:51 -0400

    Updated link paths, worked on autocomplete search input.

--- frontend/src/components/fish-finder/FishFinderNav.js
+++ frontend/src/components/fish-finder/FishFinderNav.js
@@ -1,18 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import species from '../../apis/species';
 import './css/fishFinderNav.css'
+
+const AutoComplete = () => {
+  const [searchText, setSearchText] = useState('');
+  const [debouncedSearchText, setDebouncedSearchText] = useState(searchText);
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedSearchText(searchText);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [searchText]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await species.get(`/fish-finder/search/${debouncedSearchText}`);
+      
+      if (data) {
+        setResults(data);
+      };
+    };
+
+    if (debouncedSearchText) {
+      search();
+    } else {
+      setResults([]);
+    }
+  }, [debouncedSearchText]);
+
+  const renderSearchResults = (results) => {
+    return results.map((item) => {
+      return (
+        <option value={item["Species Name"]}></option>
+      );
+    });
+  }
+
+  return (
+    <>
+      <label className="fishFinder-search-box-label" htmlFor="Search-box">Find Species:</label>
+      <input 
+        value={searchText}
+        onChange={(event) => setSearchText(event.target.value)}
+        type="search"
+        className="fishFinder-search-box"
+        list="results"
+      />
+      
+      <datalist id="results">
+        {renderSearchResults(results)}
+      </datalist>
+    </>
+  )
+}
 
 const FishFinderNav = ({ handleFilterChange }) => {
   return (
     <div className="fishFinder-nav-container">
       <div className="fishFinder-search-container">
-        <label className="fishFinder-search-box-label" htmlFor="Search-box">Find Species:</label>
-        <input className="fishFinder-search-box" type="text"/>
+        <AutoComplete />
       </div>
       <div className="fishFinder-filter-container">
-        <NavLink to="/fish-finder/profiles/all" className="fishFinder-filter-button" onClick={() => handleFilterChange('all')}>All</NavLink>
-        <NavLink to="/fish-finder/profiles/wild" className="fishFinder-filter-button" onClick={() => handleFilterChange('wild')}>Wild</NavLink>
-        <NavLink to="/fish-finder/profiles/farmed" className="fishFinder-filter-button" onClick={() => handleFilterChange('farmed')}>Farmed</NavLink>
+        <NavLink to="/fish-finder/species/all" className="fishFinder-filter-button" onClick={() => handleFilterChange('all')}>All</NavLink>
+        <NavLink to="/fish-finder/species/wild" className="fishFinder-filter-button" onClick={() => handleFilterChange('wild')}>Wild</NavLink>
+        <NavLink to="/fish-finder/species/farmed" className="fishFinder-filter-button" onClick={() => handleFilterChange('farmed')}>Farmed</NavLink>
       </div>
     </div>  
   );
