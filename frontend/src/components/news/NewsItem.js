1db1a7e31ff57d78e12007c636272a43bc6a6d74
ttolson-code <ttolson@protonmail.com>
2022-10-20 18:01:35 -0400

    Updated news items to be wrapped in a link.

--- frontend/src/components/news/NewsItem.js
+++ frontend/src/components/news/NewsItem.js
@@ -7,25 +7,25 @@ const NewsItem = (props) => {
 
   return (
     <li className="news-item">
-      <div className="news-item-content">
-        <h2 className="news-item-heading">
-          <a
-            className="news-item-link"
-            href={props.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {props.title}
-          </a>
-        </h2>
-        <p className="news-item-caption">{props.caption}</p>
-        <p className="news-item-date">{formattedDate}</p>
-      </div>
-      <img
-        className="news-item-image"
-        src={props.imageURL}
-        alt={props.title}
-      ></img>
+      <a
+        className="news-item-link"
+        href={props.url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="news-item-container">
+          <div className="news-item-content">
+            <h2 className="news-item-heading">{props.title}</h2>
+            <p className="news-item-caption">{props.caption}</p>
+            <p className="news-item-date">{formattedDate}</p>
+          </div>
+          <img
+            className="news-item-image"
+            src={props.imageURL}
+            alt={props.title}
+          ></img>
+        </div>
+      </a>
     </li>
   );
 };
