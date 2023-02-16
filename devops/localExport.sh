a5cf98f2312751110700c811619e95e3f9a1667e
ttolson-code <ttolson@protonmail.com>
2023-02-16 11:20:35 -0500

    Removed old shell scripts.

--- devops/localExport.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Script can be used to export variables if local dev is preferred over docker-compose
-
-### Variables ###
-export DB_URL="mongodb://localhost:27017/"
-export DB_NAME="seafood-app"
-export HOST_ADDRESS="localhost"
